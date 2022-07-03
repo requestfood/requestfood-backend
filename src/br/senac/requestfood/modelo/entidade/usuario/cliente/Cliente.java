40795a3d5157d3af6978fcf41f1ed36fbe4a95d5
Naudo Fellipe <106287877+naudofp@users.noreply.github.com>
2022-07-03 14:41:58 -0300

    Criada a classe Cliente
    Criei a classe cliente e também as packages: cardapio, consumivel, mesa, comanda e item.

---
+++ src/br/senac/requestfood/modelo/entidade/usuario/cliente/Cliente.java
@@ -0,0 +1,42 @@
+package br.senac.requestfood.modelo.entidade.usuario.cliente;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import br.senac.requestfood.modelo.entidade.comanda.Comanda;
+import br.senac.requestfood.modelo.entidade.usuario.Usuario;
+import br.senac.requestfood.modelo.enumeracao.genero.Genero;
+
+public class Cliente extends Usuario {
+	
+	private Genero genero;
+	private LocalDate dataNascimento;
+	private List<Comanda> comandas;
+	
+	public Cliente(int id, String nome, Contato contato, Genero genero, LocalDate dataNascimento) {
+		super(id, nome, contato);
+		setGenero(genero);
+		setDataNascimento(dataNascimento);
+		comandas = new ArrayList();
+	}
+	
+	
+	public Genero getGenero() {
+		return genero;
+	}
+	public void setGenero(Genero genero) {
+		this.genero = genero;
+	}
+	
+	public LocalDate getDataNascimento() {
+		return dataNascimento;
+	}
+	public void setDataNascimento(LocalDate dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+	
+	public List<Comanda> getComandas() {
+		return comandas;
+	}
+}
