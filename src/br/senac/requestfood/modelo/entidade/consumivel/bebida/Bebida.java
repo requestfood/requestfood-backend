01962a015660fa5d38e1c93bd07e30d32d1c675c
Luiz Santos <106398383+LuizSantos2211@users.noreply.github.com>
2022-07-04 16:41:29 -0300

    Criada a classe Bebida

--- src/br/senac/requestfood/modelo/entidade/consumivel/bebida/Bebida.java
+++ src/br/senac/requestfood/modelo/entidade/consumivel/bebida/Bebida.java
@@ -6,5 +6,33 @@ import br.senac.requestfood.modelo.enumeracao.tipobebida.TipoBebida;
 
 public class Bebida extends Consumivel{
 
+    private boolean alcoolico;
+    private TipoBebida tipoBebida;
+
+    public Bebida(String nome, float valor, String descricao, Image imagem,boolean alcoolico, TipoBebida tipoBebida){
+       super(nome, valor, descricao, imagem);
+
+        setAlcoolico(alcoolico);
+        setTipoBebida(tipoBebida);
+    }
+
+    public void setAlcoolico(boolean alcoolico) {
+        this.alcoolico = alcoolico;
+    }
+    public boolean alcoolico (boolean alcoolico) {
+        return alcoolico;
+    }
+
+    public void setTipoBebida(TipoBebida tipoBebida) {
+        this.tipoBebida = tipoBebida;
+    }
+
+    public TipoBebida getTipoBebida() {
+        return tipoBebida;
+    }
+
+
+}
+
 	
 }
