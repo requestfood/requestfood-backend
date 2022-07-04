89f716098d865d29bdfdea4ed4d6bfda60d49789
Fellipe Naudo Ferreira <106287877+naudofp@users.noreply.github.com>
2022-07-03 22:27:31 -0300

    Editado as classes "Adicionais" e "Bebida".

--- src/br/senac/requestfood/modelo/entidade/adicionais/Adicionais.java
+++ src/br/senac/requestfood/modelo/entidade/adicionais/Adicionais.java
@@ -2,25 +2,5 @@ package br.senac.requestfood.modelo.entidade.adicionais;
 
 public class Adicionais {
 
-	private int quantidadeCopos;
-	private int quantidadeTalheres;
 	
-	public Adicionais(int quantidadeCopos, int quantidadeTalheres) {
-		setQuantidadeCopos(quantidadeCopos);
-		setQuantidadeTalheres(quantidadeTalheres);
-	}
-	
-	public int getQuantidadeCopos() {
-		return quantidadeCopos;
-	}
-	public void setQuantidadeCopos(int quantidadeCopos) {
-		this.quantidadeCopos = quantidadeCopos;
-	}
-	
-	public int getQuantidadeTalheres() {
-		return quantidadeTalheres;
-	}
-	public void setQuantidadeTalheres(int quantidadeTalheres) {
-		this.quantidadeTalheres = quantidadeTalheres;
-	}
 }
--- src/br/senac/requestfood/modelo/entidade/consumivel/bebida/Bebida.java
+++ src/br/senac/requestfood/modelo/entidade/consumivel/bebida/Bebida.java
@@ -6,26 +6,5 @@ import br.senac.requestfood.modelo.enumeracao.tipobebida.TipoBebida;
 
 public class Bebida extends Consumivel{
 
-	private boolean alcoolico;
-	private TipoBebida tipoBebida;
 	
-	public Bebida(String nome, float valor, String descricao, Image imagem){
-		super(nome, valor, descricao,imagem);
-	}
-	
-	public TipoBebida getTipoBebida() {
-		return tipoBebida;
-	}
-	public void setTipoBebida(TipoBebida tipoBebida) {
-		this.tipoBebida = tipoBebida;
-	}
-
-	public boolean getAlcoolico() {
-		return alcoolico;
-	}
-	public void setAlcoolico(boolean alcoolico) {
-		this.alcoolico = alcoolico;
-	}
-
-
 }
