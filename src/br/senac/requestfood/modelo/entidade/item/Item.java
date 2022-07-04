94971794cffd7c22afe4d318d315c72c8f2a90af
Rafael Muller <mullerrafaelmr@gmail.com>
2022-07-03 21:54:39 -0300

    Criada classe Item

---
+++ src/br/senac/requestfood/modelo/entidade/item/Item.java
@@ -0,0 +1,31 @@
+package br.senac.requestfood.modelo.entidade.item;
+
+public class Item {
+
+    private int quantidade;
+    private Consumivel produto;
+    private String observacao;
+
+    public Item(int quantidade, String observacao){
+        setQuantidade(quantidade);
+        setObservacao(observacao);
+    }
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+    public int getQuantidade() {
+        return quantidade;
+    }
+    public void setProduto(Consumivel produto) {
+        this.produto = produto;
+    }
+    public Consumivel getProduto() {
+        return produto;
+    }
+    public void setObservacao(String observacao) {
+        this.observacao = observacao;
+    }
+    public String getObservacao() {
+        return observacao;
+    }
+}
