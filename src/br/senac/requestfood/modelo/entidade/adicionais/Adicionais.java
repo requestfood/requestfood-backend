c0dfbbd5a5293efadb84780932c0a6b9c2d05707
Luiz Santos <106398383+LuizSantos2211@users.noreply.github.com>
2022-07-04 16:41:54 -0300

    Criada a classe Adicionais

--- src/br/senac/requestfood/modelo/entidade/adicionais/Adicionais.java
+++ src/br/senac/requestfood/modelo/entidade/adicionais/Adicionais.java
@@ -1,6 +1,27 @@
 package br.senac.requestfood.modelo.entidade.adicionais;
 
 public class Adicionais {
+    private int quantidadeCopos;
+    private int quantidadeTalheres;
 
-	
+    public Adicionais (int quantidadeCopos, int quantidadeTalheres) {
+        setQuantidadeCopos(quantidadeCopos);
+        setQuantidadeTalheres(quantidadeTalheres);
+    }
+
+    public void setQuantidadeCopos(int quantidadeCopos) {
+        this.quantidadeCopos = quantidadeCopos;
+    }
+
+    public int getQuantidadeCopos() {
+        return quantidadeCopos;
+    }
+
+    public void setQuantidadeTalheres(int quantidadeTalheres) {
+        this.quantidadeTalheres = quantidadeTalheres;
+    }
+
+    public int getQuantidadeTalheres() {
+        return quantidadeTalheres;
+    }
 }
