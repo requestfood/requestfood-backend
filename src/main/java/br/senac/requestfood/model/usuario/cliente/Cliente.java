3166bf79f22e1010d3e6e57fc3dc88c07b7cc016
Fellipe Naudo Ferreira <106287877+naudofp@users.noreply.github.com>
2022-07-18 16:49:40 -0300

    Implementado Spring JPA na classe Cliente

--- src/main/java/br/senac/requestfood/model/usuario/cliente/Cliente.java
+++ src/main/java/br/senac/requestfood/model/usuario/cliente/Cliente.java
@@ -1,20 +1,31 @@
 package br.senac.requestfood.model.usuario.cliente;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.*;
+
 import br.senac.requestfood.model.comanda.Comanda;
 import br.senac.requestfood.model.contato.Contato;
 import br.senac.requestfood.model.genero.Genero;
 import br.senac.requestfood.model.usuario.Usuario;
 
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
-
+@Entity
+@Table(name = "cliente")
+@PrimaryKeyJoinColumn(name="id_usuario")
 public class Cliente extends Usuario {
 
+	@Enumerated(EnumType.ORDINAL)
+	@Column(name = "genero_cliente", nullable = false)
     private Genero genero;
+	
+	@Column(name = "data_nascimento_cliente", nullable = false)
     private LocalDate dataNascimento;
+	
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "cliente", cascade = CascadeType.ALL)
     private List<Comanda> comandas;
-
+	
     public Cliente(long id, String nome, Contato contato, Genero genero, LocalDate dataNascimento) {
         super(id, nome, contato);
         setGenero(genero);
