90f04b710f1e218d26840cd7873c59d93ffb35eb
Fellipe Naudo Ferreira <106287877+naudofp@users.noreply.github.com>
2022-07-18 16:49:47 -0300

    Implementado Spring JPA na classe Usuario

--- src/main/java/br/senac/requestfood/model/usuario/Usuario.java
+++ src/main/java/br/senac/requestfood/model/usuario/Usuario.java
@@ -1,14 +1,27 @@
 package br.senac.requestfood.model.usuario;
 
+import javax.persistence.*;
 import br.senac.requestfood.model.contato.Contato;
 
+@Entity
+@Table(name = "usuario")
 public abstract class Usuario {
 
+//	ATTRIBUTES
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id_usuario", nullable = false)
     private long id;
+	
+	@Column(name = "nome_usuario" , nullable = false, length = 45)
     private String nome;
+	
+	@OneToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "id_contato", nullable = false)
     private Contato contato;
 
-    // CONSTRUCTOR
+//	CONSTRUCTOR
     public Usuario (long id, String nome, Contato contato){
         setId(id);
         setNome(nome);
