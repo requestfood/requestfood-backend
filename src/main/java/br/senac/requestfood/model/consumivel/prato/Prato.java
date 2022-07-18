61c222077c29721fb7236fce756cb7e32bcac12c
Kauã Berman Amorim <kauabamorim@outlook.com>
2022-07-18 16:58:56 -0300

    Mapeada a classe Prato com Spring JPA

--- src/main/java/br/senac/requestfood/model/consumivel/prato/Prato.java
+++ src/main/java/br/senac/requestfood/model/consumivel/prato/Prato.java
@@ -4,7 +4,16 @@ import br.senac.requestfood.model.consumivel.Consumivel;
 import br.senac.requestfood.model.tipoprato.TipoPrato;
 import br.senac.requestfood.model.usuario.estabelecimento.Estabelecimento;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name="prato")
+@PrimaryKeyJoinColumn(name = "id_consumivel")
+
 public class Prato extends Consumivel {
+
+    @Enumerated(EnumType.ORDINAL)
+    @Column(name = "tipo_prato", nullable = false)
     private TipoPrato tipoPrato;
 
     public Prato(Long id, Estabelecimento estabelecimento, String nome, float valor, String descricao, Byte[] imagem, TipoPrato tipoPrato){
