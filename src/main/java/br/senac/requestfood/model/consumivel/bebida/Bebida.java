def515cb591ecdee92ac5abea81e75e3c98c40aa
Kauã Berman Amorim <kauabamorim@outlook.com>
2022-07-18 17:29:44 -0300

    Mapeada a classe Bebida com Spring JPA

--- src/main/java/br/senac/requestfood/model/consumivel/bebida/Bebida.java
+++ src/main/java/br/senac/requestfood/model/consumivel/bebida/Bebida.java
@@ -4,9 +4,18 @@ import br.senac.requestfood.model.consumivel.Consumivel;
 import br.senac.requestfood.model.tipobebida.TipoBebida;
 import br.senac.requestfood.model.usuario.estabelecimento.Estabelecimento;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "bebida")
+@PrimaryKeyJoinColumn(name = "id_consumivel")
+
 public class Bebida extends Consumivel {
 
     private boolean alcoolico;
+
+    @Enumerated(EnumType.ORDINAL)
+    @Column(name = "tipo_bebida", nullable = false)
     private TipoBebida tipoBebida;
 
     public Bebida(long id, Estabelecimento estabelecimento, String nome, float valor, String descricao, Byte[] imagem, boolean alcoolico, TipoBebida tipoBebida){
