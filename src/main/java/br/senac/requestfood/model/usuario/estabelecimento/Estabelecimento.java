712af3394e425666cd065c164129625e1aaf6b3f
Diego Trentini Pereira <diegotrentini2003@gmail.com>
2022-07-19 23:09:46 -0300

    Mapeada a classe Estabelecimento com Spring JPA

--- src/main/java/br/senac/requestfood/model/usuario/estabelecimento/Estabelecimento.java
+++ src/main/java/br/senac/requestfood/model/usuario/estabelecimento/Estabelecimento.java
@@ -5,13 +5,21 @@ import br.senac.requestfood.model.contato.Contato;
 import br.senac.requestfood.model.mesa.Mesa;
 import br.senac.requestfood.model.usuario.Usuario;
 
+import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
-
+@Entity
+@Table(name = "estabelecimento")
+@PrimaryKeyJoinColumn(name="id_usuario")
 public class Estabelecimento extends Usuario {
 
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "estabelecimento", cascade = CascadeType.ALL)
     private List<Consumivel> consumiveis;
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "estabelecimento", cascade = CascadeType.ALL)
     private List<Mesa> mesas;
+
+    public Estabelecimento() {
+    }
 
     public Estabelecimento(long id , String nome, Contato contato){
         super(id, nome, contato);
