e8dc423a03211ce7410acc9cb0adfc28c45b1707
Matheus Christo <106338239+matheuschristo@users.noreply.github.com>
2022-08-04 15:30:51 -0300

    Atualizada a interface ComandaRepository

--- src/main/java/br/senac/requestfood/repository/comanda/ComandaRepository.java
+++ src/main/java/br/senac/requestfood/repository/comanda/ComandaRepository.java
@@ -1,6 +1,32 @@
 package br.senac.requestfood.repository.comanda;
 
-public interface ComandaRepository {
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.senac.requestfood.model.comanda.Comanda;
+import br.senac.requestfood.model.item.Item;
+import br.senac.requestfood.model.mesa.Mesa;
+import br.senac.requestfood.model.usuario.cliente.Cliente;
+import br.senac.requestfood.model.usuario.estabelecimento.Estabelecimento;
+
+@Repository
+public interface ComandaRepository extends JpaRepository<Comanda, Long>{
 	//usem oque o professor mandou de exemplo
+	
+	boolean existsEstabelecimento(Estabelecimento estabelecimento);
+	
+	boolean existsCliente(Cliente cliente);
+	
+	boolean existsItem(Item item);
+	
+	boolean existsMesa(Mesa mesa);
+	
+	Optional<ComandaProjection> findById(Long id);
+	
+	Optional<ComandaMesasProjection> findComandaWithMesaById(Long id);
+	
+	
 
 }
