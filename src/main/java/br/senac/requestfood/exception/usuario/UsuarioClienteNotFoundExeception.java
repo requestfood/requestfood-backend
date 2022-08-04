1bea108c03a53d3f8f2cfa5fd2dd66d4a0691408
Luiz Santos <luiz.santos221105@gmail.com>
2022-08-04 17:04:53 -0300

    Criada as execeptions das classes cliente e estabelecimento

---
+++ src/main/java/br/senac/requestfood/exception/usuario/UsuarioClienteNotFoundExeception.java
@@ -0,0 +1,8 @@
+package br.senac.requestfood.exception.usuario;
+
+public class UsuarioClienteNotFoundExeception extends RuntimeException{
+
+    public UsuarioClienteNotFoundExeception(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/br/senac/requestfood/exception/usuario/UsuarioEstabelecimentoNotFoundExeception.java
@@ -0,0 +1,8 @@
+package br.senac.requestfood.exception.usuario;
+
+public class UsuarioEstabelecimentoNotFoundExeception extends RuntimeException{
+
+    public UsuarioEstabelecimentoNotFoundExeception(String message) {
+        super(message);
+    }
+}
