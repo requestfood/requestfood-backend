ea102c99d4b331e863754ac281a4cc6cad1b0e19
Fellipe Naudo Ferreira <106287877+naudofp@users.noreply.github.com>
2022-08-05 17:05:10 -0300

    Atualizado as classes EstabelecimentoMapper e ItemMapper

--- src/main/java/br/senac/requestfood/mapper/estabelecimento/EstabelecimentoMapper.java
+++ src/main/java/br/senac/requestfood/mapper/estabelecimento/EstabelecimentoMapper.java
@@ -1,5 +1,43 @@
 package br.senac.requestfood.mapper.estabelecimento;
 
-public class EstabelecimentoMapper {
+import java.util.ArrayList;
+import java.util.List;
 
+import org.springframework.stereotype.Service;
+
+import br.senac.requestfood.dto.estabelecimento.EstabelecimentoDTO;
+import br.senac.requestfood.model.usuario.estabelecimento.Estabelecimento;
+
+@Service
+public class EstabelecimentoMapper {
+	
+	public EstabelecimentoDTO toDTO(Estabelecimento estabelecimento) {
+		return new EstabelecimentoDTO(estabelecimento.getId(), estabelecimento.getNome(), estabelecimento.getConsumiveis(), estabelecimento.getMesas());
+	}
+	
+	public void toEntity(EstabelecimentoDTO estabelecimentoDTO) {
+	//Not Completed, waiting response for Front-End
+	}
+
+	public List<EstabelecimentoDTO> toDTO(List<Estabelecimento> estabelecimentos){
+		
+		final List<EstabelecimentoDTO> estabelecimentoDTOs = new ArrayList<>();
+		
+		for (Estabelecimento estabelecimento : estabelecimentos) {
+			estabelecimentoDTOs.add(toDTO(estabelecimento));
+		}
+		
+		return estabelecimentoDTOs;
+	}
+	
+	public List<Estabelecimento> toEntity(List<EstabelecimentoDTO> estabelecimentoDTOs) {
+		
+		final List<Estabelecimento> estabelecimentos= new ArrayList<>();
+		
+		for (EstabelecimentoDTO estabelecimentoDTO : estabelecimentoDTOs) {
+		//	estabelecimentos.add(toEntity(estabelecimentoDTO));
+		}
+		
+		return estabelecimentos;
+	}
 }
--- src/main/java/br/senac/requestfood/mapper/item/ItemMapper.java
+++ src/main/java/br/senac/requestfood/mapper/item/ItemMapper.java
@@ -1,5 +1,44 @@
 package br.senac.requestfood.mapper.item;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import br.senac.requestfood.dto.item.ItemDTO;
+import br.senac.requestfood.model.item.Item;
+
+
+@Service
 public class ItemMapper {
 
+	public ItemDTO toDTO(Item item) {
+		return new ItemDTO(item.getId(), item.getComanda(), item.getQuantidade(), item.getConsumivel(), item.getObservacao(), item.getItensAdicionais());
+	}
+	
+	public void toEntity(ItemDTO itemDTO) {
+	//Not Completed, waiting response for Front-End
+	}
+
+	public List<ItemDTO> toDTO(List<Item> items){
+		
+		final List<ItemDTO> itemDTOs = new ArrayList<>();
+		
+		for (Item item : items) {
+			itemDTOs.add(toDTO(item));
+		}
+		
+		return itemDTOs;
+	}
+	
+	public List<Item> toEntity(List<ItemDTO> itemDTOs) {
+		
+		final List<Item> items= new ArrayList<>();
+		
+		for (ItemDTO itemDTO : itemDTOs) {
+		//	items.add(toEntity(itemDTO));
+		}
+		
+		return items;
+	}
 }
