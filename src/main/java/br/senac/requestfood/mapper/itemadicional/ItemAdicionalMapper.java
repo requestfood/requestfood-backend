1289cfa6c57f79863f9181146a65a242f704d170
Kauã Berman Amorim <kauabamorim@outlook.com>
2022-08-05 17:04:05 -0300

    Atualizado a classe ItemAdicionalMapper

--- src/main/java/br/senac/requestfood/mapper/itemadicional/ItemAdicionalMapper.java
+++ src/main/java/br/senac/requestfood/mapper/itemadicional/ItemAdicionalMapper.java
@@ -1,5 +1,42 @@
 package br.senac.requestfood.mapper.itemadicional;
 
-public class ItemAdicionalMapper {
+import java.util.ArrayList;
+import java.util.List;
 
+import org.springframework.stereotype.Service;
+
+import br.senac.requestfood.dto.itemadicional.ItemAdicionalDTO;
+import br.senac.requestfood.model.itemadicional.ItemAdicional;
+
+@Service
+public class ItemAdicionalMapper {
+    public ItemAdicionalDTO toDTO(ItemAdicional itemAdicional) {
+        return new ItemAdicionalDTO(itemAdicional.getId(), itemAdicional.getItem(), itemAdicional.getNomeAdicional(), itemAdicional.getQuantidade());
+    }
+
+    public void toEntity(ItemAdicionalDTO itemAdicionalDTO) {
+		// Not completed, waiting response for Front-End
+    }
+    
+    public List<ItemAdicionalDTO> toDTO(List<ItemAdicional> itemadicionais){
+		
+		final List<ItemAdicionalDTO> itemadicionalDTOs = new ArrayList<>();
+		
+		for (ItemAdicional itemAdicional : itemadicionais) {
+			itemadicionalDTOs.add(toDTO(itemAdicional));
+		}
+		
+		return itemadicionalDTOs;
+	}
+	
+	public List<ItemAdicional> toEntity(List<ItemAdicionalDTO> itemadicionalDTOs) {
+		
+		final List<ItemAdicional> itemadicionais = new ArrayList<>();
+		
+		for (ItemAdicionalDTO itemadicionalDTO : itemadicionalDTOs) {
+//			contatos.add(toEntity(contatoDTO));
+		}
+		
+		return itemadicionais;
+	}
 }
