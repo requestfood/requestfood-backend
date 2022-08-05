cf4cecab5bc94c7eaa626c6a1770bf147f528e8e
Fellipe Naudo Ferreira <106287877+naudofp@users.noreply.github.com>
2022-08-05 16:25:40 -0300

    Criada a classe ComandaMapper

--- src/main/java/br/senac/requestfood/mapper/comanda/ComandaMapper.java
+++ src/main/java/br/senac/requestfood/mapper/comanda/ComandaMapper.java
@@ -1,5 +1,43 @@
 package br.senac.requestfood.mapper.comanda;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import br.senac.requestfood.dto.comanda.ComandaDTO;
+import br.senac.requestfood.model.comanda.Comanda;
+
+@Service
 public class ComandaMapper {
 
+	public ComandaDTO toDTO(br.senac.requestfood.model.comanda.Comanda comanda) {
+		return new ComandaDTO(comanda.getId(), comanda.getCliente(), comanda.getMesa(), comanda.getDataEmissao(), comanda.getDataFechamento(), comanda.getItens());
+	}
+	
+	public void toEntity(ComandaDTO clienteDTO) {
+	//Not Completed, waiting response for Front-End
+	}
+
+	public List<ComandaDTO> toDTO(List<Comanda> comandas){
+		
+		final List<ComandaDTO> comandaDTOs = new ArrayList<>();
+		
+		for (Comanda comanda : comandas) {
+			comandaDTOs.add(toDTO(comanda));
+		}
+		
+		return comandaDTOs;
+	}
+	
+	public List<Comanda> toEntity(List<ComandaDTO> comandaDTOs) {
+		
+		final List<Comanda> comandas= new ArrayList<>();
+		
+		for (ComandaDTO comandaDTO : comandaDTOs) {
+		//	clientes.add(toEntity(comandaDTO));
+		}
+		
+		return comandas;
+	}
 }
