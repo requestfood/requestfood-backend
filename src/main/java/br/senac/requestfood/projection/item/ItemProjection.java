cd7817221674de18656534087d76469a13ae6bcf
Luiz Santos <luiz.santos221105@gmail.com>
2022-08-05 16:10:13 -0300

    Atualizada a projection Item

--- src/main/java/br/senac/requestfood/projection/item/ItemProjection.java
+++ src/main/java/br/senac/requestfood/projection/item/ItemProjection.java
@@ -1,15 +1,9 @@
 package br.senac.requestfood.projection.item;
 
-import java.util.List;
-
 import br.senac.requestfood.model.comanda.Comanda;
 import br.senac.requestfood.model.consumivel.Consumivel;
-import br.senac.requestfood.model.item.Item;
-import br.senac.requestfood.model.itemadicional.ItemAdicional;
 
 public interface ItemProjection {
-	
-	Item getItem();
 	
 	Comanda getBill();
 	
@@ -18,7 +12,5 @@ public interface ItemProjection {
 	Consumivel getProduct();
 	
 	String getDescription();
-	
-	List<ItemAdicional> getItensAdicionais();
 
 }
