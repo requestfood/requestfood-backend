6b91cbdfd24accdb3b309b3d31516e3aa1d79185
Matheus Christo <106338239+matheuschristo@users.noreply.github.com>
2022-08-05 16:15:14 -0300

    Atualizada Projection cliente.

--- src/main/java/br/senac/requestfood/projection/cliente/ClienteProjection.java
+++ src/main/java/br/senac/requestfood/projection/cliente/ClienteProjection.java
@@ -1,18 +1,15 @@
 package br.senac.requestfood.projection.cliente;
 
-import br.senac.requestfood.projection.comanda.ComandaProjection;
-
 import java.time.LocalDate;
-import java.util.List;
 
 import br.senac.requestfood.enumeration.genero.Genero;
 
 public interface ClienteProjection {
 
+	String getNome();
+
 	Genero getGenero();
 	
 	LocalDate getDataNascimento();
-	
-	List<ComandaProjection> getComandas();
 	
 }
---
+++ src/main/java/br/senac/requestfood/projection/cliente/ClienteProjectionWithComandas.java
@@ -0,0 +1,16 @@
+package br.senac.requestfood.projection.cliente;
+
+import br.senac.requestfood.enumeration.genero.Genero;
+import br.senac.requestfood.projection.comanda.ComandaProjection;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public interface ClienteProjectionWithComandas {
+
+    Genero getGenero();
+
+    LocalDate getDataNascimento();
+
+    List<ComandaProjection> getComandas();
+}
