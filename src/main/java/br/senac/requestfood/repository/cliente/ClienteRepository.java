9c2f7287d21a277a5be08388198469c141e89a42
Fellipe Naudo Ferreira <106287877+naudofp@users.noreply.github.com>
2022-08-05 16:59:49 -0300

    Atualizado as Classes ClienteRepository e ComandaRepository

--- src/main/java/br/senac/requestfood/repository/cliente/ClienteRepository.java
+++ src/main/java/br/senac/requestfood/repository/cliente/ClienteRepository.java
@@ -1,9 +1,10 @@
 package br.senac.requestfood.repository.cliente;
 
-import br.senac.requestfood.enumeration.genero.Genero;
-import br.senac.requestfood.model.usuario.cliente.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import br.senac.requestfood.enumeration.genero.Genero;
+import br.senac.requestfood.model.usuario.cliente.Cliente;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Long> {
--- src/main/java/br/senac/requestfood/repository/comanda/ComandaRepository.java
+++ src/main/java/br/senac/requestfood/repository/comanda/ComandaRepository.java
@@ -1,12 +1,9 @@
 package br.senac.requestfood.repository.comanda;
 
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import br.senac.requestfood.model.comanda.Comanda;
-import br.senac.requestfood.model.item.Item;
 import br.senac.requestfood.model.mesa.Mesa;
 import br.senac.requestfood.model.usuario.cliente.Cliente;
 import br.senac.requestfood.model.usuario.estabelecimento.Estabelecimento;
