6334dd597c2e78cd99fd19e7689a272c732b9016
Fellipe Naudo Ferreira <106287877+naudofp@users.noreply.github.com>
2022-08-05 17:09:21 -0300

    Atualizado a classe PratoMapper

--- src/main/java/br/senac/requestfood/mapper/prato/PratoMapper.java
+++ src/main/java/br/senac/requestfood/mapper/prato/PratoMapper.java
@@ -1,5 +1,43 @@
 package br.senac.requestfood.mapper.prato;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import br.senac.requestfood.dto.prato.PratoDTO;
+import br.senac.requestfood.model.consumivel.prato.Prato;
+
+@Service
 public class PratoMapper {
 
+	public PratoDTO toDTO(Prato prato) {
+		return new PratoDTO(prato.getId(), prato.getNome(), prato.getValor());
+	}
+	
+	public void toEntity(PratoDTO pratoDTO) {
+	//Not Completed, waiting response for Front-End
+	}
+
+	public List<PratoDTO> toDTO(List<Prato> pratos){
+		
+		final List<PratoDTO> pratoDTOs = new ArrayList<>();
+		
+		for (Prato prato : pratos) {
+			pratoDTOs.add(toDTO(prato));
+		}
+		
+		return pratoDTOs;
+	}
+	
+	public List<Prato> toEntity(List<PratoDTO> pratoDTOs) {
+		
+		final List<Prato> pratos= new ArrayList<>();
+		
+		for (PratoDTO pratoDTO : pratoDTOs) {
+		//	clientes.add(toEntity(clienteDTO));
+		}
+		
+		return pratos;
+	}
 }
