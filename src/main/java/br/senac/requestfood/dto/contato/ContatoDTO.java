751ffa32e483fcde87564be449c1e9d606baf7e9
Kauã Berman Amorim <kauabamorim@outlook.com>
2022-08-05 16:41:14 -0300

    Atualizado a classe ContatoMapper

--- src/main/java/br/senac/requestfood/dto/contato/ContatoDTO.java
+++ src/main/java/br/senac/requestfood/dto/contato/ContatoDTO.java
@@ -1,5 +1,3 @@
 package br.senac.requestfood.dto.contato;
 
-import br.senac.requestfood.model.usuario.Usuario;
-
-public record ContatoDTO(Long id, Usuario usuario, String telefone, String email) {}
+public record ContatoDTO(Long id, String telefone, String email) {}
--- src/main/java/br/senac/requestfood/mapper/contato/ContatoMapper.java
+++ src/main/java/br/senac/requestfood/mapper/contato/ContatoMapper.java
@@ -1,5 +1,43 @@
 package br.senac.requestfood.mapper.contato;
 
-public class ContatoMapper {
+import java.util.ArrayList;
+import java.util.List;
 
+import org.springframework.stereotype.Service;
+
+import br.senac.requestfood.dto.contato.ContatoDTO;
+import br.senac.requestfood.model.contato.Contato;
+
+@Service
+public class ContatoMapper {
+	
+	public ContatoDTO toDTO(Contato contato) {
+		return new ContatoDTO(contato.getId(), contato.getTelefone(), contato.getEmail());
+	}
+	
+	public void toEntity(ContatoDTO contatoDTO) {
+		// Not completed, waiting response for Front-End
+	}
+	
+	public List<ContatoDTO> toDTO(List<Contato> contatos){
+		
+		final List<ContatoDTO> contatoDTOs = new ArrayList<>();
+		
+		for (Contato contato : contatos) {
+			contatoDTOs.add(toDTO(contato));
+		}
+		
+		return contatoDTOs;
+	}
+	
+	public List<Contato> toEntity(List<ContatoDTO> contatoDTOs) {
+		
+		final List<Contato> contatos = new ArrayList<>();
+		
+		for (ContatoDTO contatoDTO : contatoDTOs) {
+//			contatos.add(toEntity(contatoDTO));
+		}
+		
+		return contatos;
+	}
 }
