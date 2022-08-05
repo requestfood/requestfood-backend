2f319b377e6e7d6541678b1839001f39c5cfb267
Kauã Berman Amorim <kauabamorim@outlook.com>
2022-08-05 13:50:20 -0300

    Criada Exception Password da classe Usuario

---
+++ src/main/java/br/senac/requestfood/exception/usuario/UsuarioPasswordRegisteredException.java
@@ -0,0 +1,8 @@
+package br.senac.requestfood.exception.usuario;
+
+public class UsuarioPasswordRegisteredException extends RuntimeException{
+	public UsuarioPasswordRegisteredException(String message) {
+		super(message);
+	}
+
+}
