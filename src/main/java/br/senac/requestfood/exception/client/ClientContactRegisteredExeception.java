f97e717cb128715ca8ff348f81cf6ed0ef2a0fe8
Matheus Christo <matheuschristo.09@gmail.com>
2022-08-06 14:58:18 -0300

    Atualizado exceptions cliente.

---
+++ src/main/java/br/senac/requestfood/exception/client/ClientContactRegisteredExeception.java
@@ -0,0 +1,9 @@
+package br.senac.requestfood.exception.client;
+
+public class ClientContactRegisteredExeception extends RuntimeException{
+
+	public ClientContactRegisteredExeception(String message) {
+		
+		super(message);
+	}
+}
