c1420b48c1b30ef9828c0d1060df44a3c793e18f
Matheus Christo <matheuschristo.09@gmail.com>
2022-08-06 15:49:25 -0300

    Alterado a ComandaDTO.

--- src/main/java/br/senac/requestfood/dto/comanda/ComandaDTO.java
+++ src/main/java/br/senac/requestfood/dto/comanda/ComandaDTO.java
@@ -7,4 +7,4 @@ import br.senac.requestfood.model.item.Item;
 import br.senac.requestfood.model.mesa.Mesa;
 import br.senac.requestfood.model.usuario.cliente.Cliente;
 
-public record ComandaDTO(Long id, Cliente cliente, Mesa mesa, LocalDateTime dataEmissao, LocalDateTime dataFechamento, List<Item> itens) {}
+public record ComandaDTO(Long id, Cliente cliente, Mesa mesa, LocalDateTime dataEmissao, LocalDateTime dataFechamento, List<Item> itens, Double valorTotal) {}
