d33c48837e8253e914e537d8080f7718224980cf
Vinicius Rezende <vinirezende60@gmail.com>
2022-08-07 02:18:17 -0300

    Atualizado a interface ClienteRepository

--- src/main/java/br/senac/requestfood/repository/cliente/ClienteRepository.java
+++ src/main/java/br/senac/requestfood/repository/cliente/ClienteRepository.java
@@ -6,10 +6,19 @@ import org.springframework.stereotype.Repository;
 import br.senac.requestfood.enumeration.genero.Genero;
 import br.senac.requestfood.model.usuario.cliente.Cliente;
 
+import java.time.LocalDate;
+
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Long> {
 
+
+    boolean existsName(String name);
+    boolean existsContato (String contato);
+
+    boolean existsPassword (String password);
+
     boolean existsGenero (Genero genero);
+
 
 
 
