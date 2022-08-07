80d0a44444636002d330cc9016133a475ff52d12
Vinicius Rezende <vinirezende60@gmail.com>
2022-08-07 02:23:32 -0300

    Atualizado a interface MesaRepository

--- src/main/java/br/senac/requestfood/repository/mesa/MesaRepository.java
+++ src/main/java/br/senac/requestfood/repository/mesa/MesaRepository.java
@@ -3,16 +3,26 @@ package br.senac.requestfood.repository.mesa;
 import br.senac.requestfood.model.mesa.Mesa;
 import br.senac.requestfood.model.usuario.Usuario;
 import br.senac.requestfood.model.usuario.estabelecimento.Estabelecimento;
+import br.senac.requestfood.projection.mesa.TableProjection;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface MesaRepository extends JpaRepository<Mesa, Long> {
     //usem oque o professor mandou de exemplo
 
-    boolean existsUsuario(Usuario usuario);
+    boolean existsId (Long id);
+
+    boolean existsPassword (String password);
+    boolean existsLimitUserNumber(Integer limitUserNumber);
 
     boolean existsEstabelecimento(Estabelecimento estabelecimento);
+
+    @Query(value = "SELECT u.id AS id, u.name AS name, u.email AS email FROM University u")
+    List<TableProjection> findComandas();
 
 }
 
