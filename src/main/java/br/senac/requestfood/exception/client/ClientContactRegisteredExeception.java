f86288b4a613c9c8f39851d31a712b3aa3d98fd0
Matheus Christo <matheuschristo.09@gmail.com>
2022-08-07 22:05:10 -0300

    Atualizado ContactRegisteredExeception.

--- src/main/java/br/senac/requestfood/exception/client/ClientContactRegisteredExeception.java
+++
@@ -1,9 +0,0 @@
-package br.senac.requestfood.exception.client;
-
-public class ClientContactRegisteredExeception extends RuntimeException{
-
-	public ClientContactRegisteredExeception(String message) {
-		
-		super(message);
-	}
-}
---
+++ src/main/java/br/senac/requestfood/exception/client/ContactRegisteredExeception.java
@@ -0,0 +1,9 @@
+package br.senac.requestfood.exception.client;
+
+public class ContactRegisteredExeception extends RuntimeException{
+
+	public ContactRegisteredExeception(String message) {
+		
+		super(message);
+	}
+}
