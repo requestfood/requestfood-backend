9d64ad5a0a72402fe65b681a031c6afc375204b3
Fellipe Naudo Ferreira <106287877+naudofp@users.noreply.github.com>
2022-08-07 23:32:48 -0300

    Atualizado ItemService e ItemServiceImpl

--- src/main/java/br/senac/requestfood/service/item/ItemService.java
+++ src/main/java/br/senac/requestfood/service/item/ItemService.java
@@ -1,5 +1,22 @@
 package br.senac.requestfood.service.item;
 
+import java.util.List;
+
+import br.senac.requestfood.dto.item.ItemDTO;
+import br.senac.requestfood.projection.item.ItemProjection;
+import br.senac.requestfood.projection.item.ItemWithItemAdditionalProjection;
+
 public interface ItemService {
 
+	ItemDTO save(ItemDTO itemDTO);
+	
+	void update(ItemDTO itemDTO, Long id);
+	
+	void delete(Long id);
+	
+	ItemProjection findById(Long id);
+	
+	ItemWithItemAdditionalProjection findItemWithItemAdditionalById(Long id);
+	
+	List<ItemProjection> findAll();
 }
---
+++ src/main/java/br/senac/requestfood/service/item/ItemServiceImpl.java
@@ -0,0 +1,67 @@
+package br.senac.requestfood.service.item;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import br.senac.requestfood.dto.item.ItemDTO;
+import br.senac.requestfood.exception.item.ItemNotFoundException;
+import br.senac.requestfood.mapper.item.ItemMapper;
+import br.senac.requestfood.model.item.Item;
+import br.senac.requestfood.projection.item.ItemProjection;
+import br.senac.requestfood.projection.item.ItemWithItemAdditionalProjection;
+import br.senac.requestfood.repository.item.ItemRepository;
+
+@Service
+public class ItemServiceImpl implements ItemService{
+
+	private final ItemRepository repository;
+	private final ItemMapper mapper;
+
+	public ItemServiceImpl(ItemRepository repository, ItemMapper mapper) {
+		this.repository = repository;
+		this.mapper = mapper;
+	}
+
+	public ItemDTO save(ItemDTO itemDTO) {
+		
+		Item item = mapper.toEntity(itemDTO); 
+		Item itemSaved = repository.save(item);
+		
+		return mapper.toDTO(itemSaved);
+	}
+
+	public void update(ItemDTO itemDTO, Long id) {
+		
+		Item item = repository.findById(id).orElseThrow(() -> new ItemNotFoundException("Item "+ id +" was not found"));		
+		
+		item.setId(itemDTO.id());
+		item.setComanda(itemDTO.comanda());
+		item.setQuantidade(itemDTO.quantidade());
+		item.setConsumivel(itemDTO.consumivel());
+		item.setObservacao(itemDTO.observacao());
+	}
+
+	public void delete(Long id) {
+		
+		if(!repository.existsById(id))
+			throw new ItemNotFoundException("Item "+ id +" was not found");
+		
+		repository.deleteById(id);
+	}
+
+	public ItemProjection findById(Long id) {
+
+		ItemProjection item = repository.findItemById(id).orElseThrow(() -> new ItemNotFoundException("Item " + id + " was not found"));
+		return item;
+	}
+
+	public ItemWithItemAdditionalProjection findItemWithItemAdditionalById(Long id) {
+		ItemWithItemAdditionalProjection item = repository.findItemWithItemAdditionalById(id).orElseThrow(() -> new ItemNotFoundException("ItemAddicional " + id + " was not found"));
+		return item;
+	}
+
+	public List<ItemProjection> findAll() {
+		return repository.findItems();
+	}
+}
