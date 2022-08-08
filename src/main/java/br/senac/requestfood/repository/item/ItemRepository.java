79d5d15acb2bc92a5cbc7203770fc8858c5e30b9
Fellipe Naudo Ferreira <106287877+naudofp@users.noreply.github.com>
2022-08-07 23:32:17 -0300

    Atualizado ItemRepository

--- src/main/java/br/senac/requestfood/repository/item/ItemRepository.java
+++ src/main/java/br/senac/requestfood/repository/item/ItemRepository.java
@@ -1,34 +1,23 @@
 package br.senac.requestfood.repository.item;
 
-import br.senac.requestfood.model.comanda.Comanda;
-import br.senac.requestfood.model.consumivel.Consumivel;
-import br.senac.requestfood.model.item.Item;
-import br.senac.requestfood.model.itemadicional.ItemAdicional;
-import br.senac.requestfood.projection.itemAdicional.ItemAdicionalProjection;
-import br.senac.requestfood.repository.itemadicional.ItemAdicionalRepository;
-import org.springframework.data.domain.Example;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
+import br.senac.requestfood.model.item.Item;
+import br.senac.requestfood.projection.item.ItemProjection;
+import br.senac.requestfood.projection.item.ItemWithItemAdditionalProjection;
 
 @Repository
 public interface ItemRepository extends JpaRepository<Item, Long> {
-	//usem oque o professor mandou de exemplo
 
-
-
-  boolean existsBill (Comanda bill);
-  boolean existsQuantity (Integer Quantity);
-  boolean existsItemAdicional (ItemAdicional itemAdicional);
-
-  boolean existsProduct (Consumivel product);
-
-  boolean existsDescription (String Description);
-
-  @Query(value = "SELECT u.id AS id, u.name AS name FROM Estabelecimento u")
-  List<ItemAdicionalProjection> findItem();
-
-
+	Optional<ItemWithItemAdditionalProjection> findItemWithItemAdditionalById(Long id);
+	
+	Optional<ItemProjection> findItemById(Long id);
+	
+	@Query(value = "SELECT i.id_item AS id, i.id_command AS command, i.quantity_item AS quantity, i.id_consumable AS consumable, i.observation_item AS observation FROM Item i")
+	List<ItemProjection> findItems();
 }
