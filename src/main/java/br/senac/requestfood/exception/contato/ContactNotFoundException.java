3ab3b350c0e2a7daad2b570e89a47ae4d6998892
Matheus Christo <matheuschristo.09@gmail.com>
2022-08-07 22:52:49 -0300

    Criado ContactNotFoundException.

---
+++ src/main/java/br/senac/requestfood/exception/contato/ContactNotFoundException.java
@@ -0,0 +1,9 @@
+package br.senac.requestfood.exception.contato;
+
+public class ContactNotFoundException extends RuntimeException{
+
+	public ContactNotFoundException(String message) {
+	
+		super(message);
+	}
+}
