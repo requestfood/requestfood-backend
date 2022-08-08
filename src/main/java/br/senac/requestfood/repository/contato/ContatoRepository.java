7f3510201ee090f1a4263da30b329ae325c0c7b0
Matheus Christo <matheuschristo.09@gmail.com>
2022-08-07 22:53:23 -0300

    Atualizado ContatoRepository.

--- src/main/java/br/senac/requestfood/repository/contato/ContatoRepository.java
+++ src/main/java/br/senac/requestfood/repository/contato/ContatoRepository.java
@@ -2,16 +2,27 @@ package br.senac.requestfood.repository.contato;
 
 import br.senac.requestfood.model.contato.Contato;
 import br.senac.requestfood.model.usuario.Usuario;
+import br.senac.requestfood.projection.cliente.ClienteProjection;
+import br.senac.requestfood.projection.contato.ContatoProjection;
+
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ContatoRepository extends JpaRepository<Contato, Long> {
 
-    boolean existsEmail (String email);
-    boolean existsTelefone (String Telefone);
-    boolean existsUsuario (Usuario usuario);
+    boolean existsByEmail (String email);
+    
+    boolean existsByTelefone (String Telefone);
+    
+    boolean existsByUsuario (Usuario usuario);
 
-
-	
+    Optional<ContatoProjection> findContactById(Long id);
+    
+    @Query(value = "SELECT c.id_user AS id, c.name_user AS name, c.id_contact AS contact, c.password.user AS password, c.genero_client AS genero, c.data_nascimento_cliente AS data_nascimento FROM cliente c")
+    List<ContatoProjection> findContacts();
 }
