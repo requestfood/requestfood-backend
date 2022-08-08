68434d60301eb965505ececf2f7a2a98fed20a12
Fellipe Naudo Ferreira <106287877+naudofp@users.noreply.github.com>
2022-08-07 21:34:09 -0300

    Atualizado DrinkServiceImpl e DrinkService

--- src/main/java/br/senac/requestfood/service/drink/DrinkService.java
+++ src/main/java/br/senac/requestfood/service/drink/DrinkService.java
@@ -1,5 +1,19 @@
 package br.senac.requestfood.service.drink;
 
+import java.util.List;
+
+import br.senac.requestfood.dto.bebida.BebidaDTO;
+import br.senac.requestfood.projection.bebida.BebidaProjection;
+
 public interface DrinkService {
 
+	BebidaDTO save(BebidaDTO bebidaDTO);
+
+	void update(BebidaDTO bebidaDTO, Long id);
+
+	void delete(Long id);
+	
+	BebidaProjection findById(Long id);
+	
+	List<BebidaProjection> findAll();
 }
---
+++ src/main/java/br/senac/requestfood/service/drink/DrinkServiceImpl.java
@@ -0,0 +1,67 @@
+package br.senac.requestfood.service.drink;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import br.senac.requestfood.dto.bebida.BebidaDTO;
+import br.senac.requestfood.exception.consumivel.ConsumivelNameRegisteredException;
+import br.senac.requestfood.exception.consumivel.ConsumivelNotFoundException;
+import br.senac.requestfood.mapper.bebida.BebidaMapper;
+import br.senac.requestfood.model.consumivel.bebida.Bebida;
+import br.senac.requestfood.projection.bebida.BebidaProjection;
+import br.senac.requestfood.repository.bebida.BebidaRepository;
+
+@Service
+public class DrinkServiceImpl implements DrinkService{
+
+	private final BebidaRepository repository;
+	private final BebidaMapper mapper;
+
+	public DrinkServiceImpl(BebidaRepository repository, BebidaMapper mapper) {
+		this.repository = repository;
+		this.mapper = mapper;
+	}
+	
+	public BebidaDTO save(BebidaDTO bebidaDTO) {
+
+		if (!repository.existsByName(bebidaDTO.nome()))
+			throw new ConsumivelNameRegisteredException("Bebida " + bebidaDTO.nome() + " is already registered");
+
+		Bebida drink = mapper.toEntity(bebidaDTO);
+		Bebida drinkSaved = repository.save(drink);
+
+		return mapper.toDTO(drinkSaved);
+	}
+
+	public void update(BebidaDTO bebidaDTO, Long id) {
+
+		Bebida drink = repository.findById(id).orElseThrow(() -> new ConsumivelNotFoundException("Drink " + id + " was not found"));
+
+		if (!repository.existsByName(bebidaDTO.nome()))
+			throw new ConsumivelNameRegisteredException("Bebida " + bebidaDTO.nome() + " is already registered");
+
+		drink.setEstabelecimento(bebidaDTO.estabelecimento());
+		drink.setNome(bebidaDTO.nome());
+		drink.setValor(bebidaDTO.valor());
+
+		repository.save(drink);
+	}
+
+	public void delete(Long id) {
+
+		if (!repository.existsById(id))
+			throw new ConsumivelNotFoundException("Drink " + id + " was not found");
+
+			repository.deleteById(id);
+	}
+
+	public BebidaProjection findById(Long id) {
+		BebidaProjection bebida = repository.findDrinkById(id).orElseThrow(() -> new ConsumivelNotFoundException("Drink " + id + " was not found"));
+		return bebida;
+	}
+
+	public List<BebidaProjection> findAll() {
+		return repository.findDrinks();
+	}
+}
