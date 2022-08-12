e2ffccd5c9209ff6c4565b27452eb7799216e230
Diego Trentini Pereira <diegotrentini2003@gmail.com>
2022-08-12 17:15:10 -0300

    Atualizado Repository Command

--- src/main/java/br/senac/requestfood/repository/command/CommandRepository.java
+++ src/main/java/br/senac/requestfood/repository/command/CommandRepository.java
@@ -21,6 +21,6 @@ public interface CommandRepository extends JpaRepository<Command, Long>{
 
 	Optional<CommandWithClosureDateProjection> findCommandWithClosureDate(Long id);
 	
-	@Query(value = "SELECT c.id AS id, c.client AS client, c.issue_date_command AS issueDate, c.closing_date_command AS closingDate FROM Command c")
+	@Query(value = "SELECT c.id AS id, c.client AS client, c.IssueDate AS issueDate, c.closingDate AS closingDate FROM Command c")
 	List<CommandProjection> findCommands();
 }
