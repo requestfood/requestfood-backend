498662bd4a8669d39ddfdd130d9161a453c75871
Fellipe Naudo Ferreira <felipenaudof@gmail.com>
2022-08-12 15:00:47 -0300

    Adicionado atributo image a entidade Establishment

--- src/main/java/br/senac/requestfood/model/user/establishment/Establishment.java
+++ src/main/java/br/senac/requestfood/model/user/establishment/Establishment.java
@@ -3,8 +3,10 @@ package br.senac.requestfood.model.user.establishment;
 import java.util.List;
 
 import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
+import javax.persistence.Lob;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
@@ -17,29 +19,36 @@ import br.senac.requestfood.model.user.User;
 public class Establishment extends User {
 
     @OneToMany(fetch = FetchType.LAZY, mappedBy = "establishment", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<Consumable> consumable;
+    private List<Consumable> consumables;
     
     @OneToMany(fetch = FetchType.LAZY, mappedBy = "establishment", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Command> commands;
+    
+    @Lob
+	@Column(name = "image_establishment")
+	private Byte[] image;
 
     public Establishment() {}
     
 	public Establishment(List<Consumable> consumable, List<Command> commands) {
-		this.consumable = consumable;
+		this.consumables = consumable;
 		this.commands = commands;
 	}
 
 	public List<Consumable> getConsumables() {
-		return consumable;
+		return consumables;
 	}
-	public void setConsumables(List<Consumable> consumable) {
-		this.consumable = consumable;
-	}
-
 	public List<Command> getCommands() {
 		return commands;
 	}
 	public void setCommands(List<Command> commands) {
 		this.commands = commands;
 	}
+	public Byte[] getImage() {
+		return image;
+	}
+	public void setImage(Byte[] image) {
+		this.image = image;
+	}
+	
 }
