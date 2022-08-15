f3d462fa4031cbd99b7f9092dae5d96efb8c8c05
Diego Trentini Pereira <diegotrentini2003@gmail.com>
2022-08-15 17:18:37 -0300

    Atualizado ItemRepository.

--- src/main/java/br/senac/requestfood/repository/item/ItemRepository.java
+++ src/main/java/br/senac/requestfood/repository/item/ItemRepository.java
@@ -15,6 +15,6 @@ public interface ItemRepository extends JpaRepository<Item, Long> {
 
 	Optional<ItemProjection> findItemById(Long id);
 	
-	@Query(value = "SELECT i.id_item AS id, i.id_order AS order, i.quantity_item AS quantity, i.id_consumable AS consumable, i.observation_item AS observation FROM Item i")
+	@Query(value = "SELECT i.id AS id, i.order AS order, i.quantity AS quantity, i.consumable AS consumable, i.observation AS observation FROM Item i")
 	List<ItemProjection> findItems();
 }
