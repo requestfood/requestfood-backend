da3021c20248fcff86505fa47dbc509000befa3e
Fellipe Naudo <106287877+naudofp@users.noreply.github.com>
2022-08-21 00:22:16 -0300

    Criada Exceptions ClientEmailNotFoundException e EstablishmentEmailNotFoundException

---
+++ src/main/java/br/senac/requestfood/exception/establishment/EstablishmentEmailNotFoundException.java
@@ -0,0 +1,8 @@
+package br.senac.requestfood.exception.establishment;
+
+public class EstablishmentEmailNotFoundException extends RuntimeException {
+	
+	public EstablishmentEmailNotFoundException(String message) {
+		super(message);
+	}
+}
