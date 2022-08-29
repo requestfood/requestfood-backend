675054762b86bbe9914ed19ed60cb1fb2a6cf0f4
Fellipe Naudo Ferreira <106287877+naudofp@users.noreply.github.com>
2022-08-29 17:11:11 -0300

    Criada classe ConsumableRepository

---
+++ src/main/java/br/senac/requestfood/repository/consumable/ConsumableRepository.java
@@ -0,0 +1,21 @@
+package br.senac.requestfood.repository.consumable;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import br.senac.requestfood.model.consumable.Consumable;
+import br.senac.requestfood.projection.consumable.ConsumableProjection;
+import br.senac.requestfood.projection.dish.DishProjection;
+
+public interface ConsumableRepository extends JpaRepository<Consumable, Long>{
+	
+    Optional<ConsumableProjection> findConsumableById(Long id);
+
+    Optional<ConsumableProjection> findByName(String name);
+    
+    @Query(value = "SELECT c.id AS id, c.name AS name, c.establishment AS establishment, c.price AS price, c.description AS description, c.image AS image FROM Consumable c")
+    List<DishProjection> findDishes();
+}
