a3f934a10e97ec2a09f8e6f89804e5d2dd4feaa1
Fellipe Naudo <106287877+naudofp@users.noreply.github.com>
2022-09-26 22:12:33 -0300

    Atualizado Enums OrderStatus, CategoryDish

--- src/main/java/br/senac/requestfood/dto/dish/DishDTO.java
+++ src/main/java/br/senac/requestfood/dto/dish/DishDTO.java
@@ -2,4 +2,4 @@ package br.senac.requestfood.dto.dish;
 
 import br.senac.requestfood.enumeration.dish.CategoryDish;
 
-public record DishDTO(Long id, Long idEstablishment, String name, String description, Byte[] image, Double price, CategoryDish typeDish) {}
+public record DishDTO(Long id, Long idEstablishment, String name, String description, Byte[] image, Double price, CategoryDish categoryDish) {}
