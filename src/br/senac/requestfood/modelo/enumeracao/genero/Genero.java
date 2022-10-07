a464fe9058448785d5d4da40ce9339b7dc9638fa
Fellipe Naudo <106287877+naudofp@users.noreply.github.com>
2022-10-06 23:02:52 -0300

    Organizado pastas do projeto

--- src/br/senac/requestfood/modelo/enumeracao/genero/Genero.java
+++
@@ -1,6 +0,0 @@
-package br.senac.requestfood.modelo.enumeracao.genero;
-
-public enum Genero {
-
-	MASCULINO, FEMININO, OUTRO;
-}
--- src/br/senac/requestfood/modelo/enumeracao/tipoprato/TipoPrato.java
+++
@@ -1,6 +0,0 @@
-package br.senac.requestfood.modelo.enumeracao.tipoprato;
-
-public enum TipoPrato {
-
-    PRONTO, SOBREMESA, PORCAO
-}
